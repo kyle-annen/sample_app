a8cc119656f4855649f6ca3880563b60c1d4651a
Kyle Annen <kyleannen@gmail.com>
2015-06-24 23:05:47 -0700

    Finish login/logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624174223) do
+ActiveRecord::Schema.define(version: 20150625053145) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150624174223) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
